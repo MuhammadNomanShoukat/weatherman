3fed9805061cfd5dfe7c94f1749b81953db7e973
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2019-09-16 07:50:37 -0700

    Committed done.
    In that file, I have set the user base directory, he can make his own directory and make a file in it. but some changes have not done. I have left it to make them better in further process.

--- weather_app/management/commands/load_files_data.py
+++ weather_app/management/commands/load_files_data.py
@@ -1,23 +1,32 @@
-from django.core.management.base import BaseCommand
 import os
-import csv
-from .utils import reading_files,data_sending_to_database
 
-""" Base Command class  """
+from django.core.management.base import BaseCommand
+
+from .utils import data_sending_to_database, reading_files
+
+
 class Command(BaseCommand):
     help = "Loading data from files"
-    """ 
-    command function handling here actions
-    1: reading_files()
-    2: data_sending_to_database()
-    both function are using in it
+
+    """ using for weather file argument '"""
+    def add_arguments(self, parser):
+        parser.add_argument('file_name', type=str, help='Weather file folder name')
+
+    """ command function handling here actions
+        1: reading_files()
+        2: data_sending_to_database()
+        both function are using in it
     """
+
     def handle(self, *args, **options):
         path_root = os.path.abspath(os.path.dirname(__name__))
-        folder_name = "weatherfiles"
-        path = path_root + "\\" + folder_name
-        """ using for reading files form directory and returning back to files """
-        files = reading_files(path)
-        """ using for sending data to the database """
-        data_sending_to_database(files)
+        folder_name = options['file_name']
 
+        """ checking here if directory is already exist or not"""
+        if not os.path.isdir(folder_name):
+            os.mkdir(os.path.join(path_root, folder_name))
+
+        else:
+            path = path_root + "\\" + folder_name
+            files = reading_files(path)
+            data_sending_to_database(files)
