f7e64d658eec8ab84e5edbda0d6b2bb7f32bbc43
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2019-09-16 07:44:29 -0700

    Committed done.
    I've tried to make a custom filter, but I don't understand how to make it. I'm trying to make it as soon as possible. There are some piece of code on the internet but I don't know how to handle them. You can se in the commented code.

--- weather_app/admin.py
+++ weather_app/admin.py
@@ -1,6 +1,54 @@
 from django.contrib import admin
-from .models import WeatherCharacteristic,Weather
+from django.contrib.admin import SimpleListFilter
 
+from .models import Weather, WeatherCharacteristic
+
+
+# class SpeciesListFilter(admin.SimpleListFilter):
+#     title = 'weather'
+#     parameter_name = 'weather'
+#
+#     default_value = None
+#
+#     def lookups(self, request, model_admin):
+#
+#         list_of_species = []
+#         queryset = Weather.objects.all()
+#         for species in queryset:
+#
+#             list_of_species.append(
+#                 (str(species.id), species.weather_date)
+#             )
+#         return sorted(list_of_species)
+#
+#     def queryset(self, request, queryset):
+#
+#
+#         if self.value():
+#             return queryset.filter(id=self.value())
+#         return queryset
+#
+#
+#
+#     def value(self):
+#
+#         value = super(SpeciesListFilter, self).value()
+#         if value is None:
+#             if self.default_value is None:
+#                 # If there is at least one Species, return the first by name. Otherwise, None.
+#                 first_species = Weather.objects.order_by('temperature').first()
+#                 value = None if first_species is None else first_species.id
+#                 self.default_value = value
+#             else:
+#                 value = self.default_value
+#         return str(value)
+#
+#
+# @admin.register(Weather)
+# class BreedAdmin(admin.ModelAdmin):
+#     list_display = ('weather_date', 'temperature','humidity','dew','wind_velocity')
+#     list_filter = (SpeciesListFilter,)
+#
 
 class WeatherCharacteristicAdminClass(admin.ModelAdmin):
 
@@ -10,11 +58,6 @@ class WeatherCharacteristicAdminClass(admin.ModelAdmin):
         'avg_value',
     ]
 
-    list_filter = [
-        'max_value',
-        'min_value',
-        'avg_value',
-    ]
 
 class WeatherUserAdmin(admin.ModelAdmin):
     list_display = [
@@ -22,6 +65,11 @@ class WeatherUserAdmin(admin.ModelAdmin):
         'temperature',
         'humidity',
     ]
+
+    list_filter = [
+        'weather_date',
+    ]
+
 
 admin.site.register(WeatherCharacteristic,WeatherCharacteristicAdminClass)
 admin.site.register(Weather,WeatherUserAdmin)
